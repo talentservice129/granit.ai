5b774f92b9e1619958362d6096f1f686f85ceff7
talentservice129 <talentservice129@gmail.com>
2024-06-12 09:01:09 +0300

    kpis & smo

--- Skote_Flask_v2.1.0/Admin/skote/pages.py
+++ Skote_Flask_v2.1.0/Admin/skote/pages.py
@@ -1,6 +1,7 @@
 from flask import Blueprint,render_template, request
 from flask_login import login_required
 import pandas as pd
+from datetime import datetime
 
 pages = Blueprint('page',__name__,template_folder='templates',
     static_folder='static',)
@@ -20,7 +21,7 @@ def page_main():
         latest = df.pivot_table(index=['RCS_ID', date_column], columns='DP_Name', values='DP_Value', aggfunc='first').reset_index()
         latest[date_column] = pd.to_datetime(latest[date_column],format='mixed')
         latest = latest.sort_values(by=date_column, ascending=False)
-        latest[date_column] = latest[date_column].dt.strftime('%m/%d/%Y')
+        latest[date_column] = datetime.now().date().strftime('%m/%d/%Y')
     else:
         pivot_df = df.pivot_table(index=['RCS_ID', date_column], columns='DP_Name', values='DP_Value', aggfunc='first').reset_index()
         pivot_df[date_column] = pd.to_datetime(pivot_df[date_column],format='mixed')
@@ -30,12 +31,10 @@ def page_main():
         for index, row in latest.iterrows():
             filtered = df_sorted[(df_sorted['RCS_ID'] == row['RCS_ID']) & ((df_sorted['Capital Fixed'] == 'TRUE') | (df_sorted['Capital Variable'] == 'TRUE'))]
 
-            print(filtered.iloc[0]['Capital amount'])
-
             if filtered.empty == False:
                 latest.at[index, 'Capital Fixed'] = filtered.iloc[0]['Capital Fixed']
                 latest.at[index, 'Capital Variable'] = filtered.iloc[0]['Capital Variable']
-                latest.at[index, 'Capital amount'] = filtered.iloc[0]['Capital amount']
+                # latest.at[index, 'Capital amount'] = filtered.iloc[0]['Capital amount']
 
             filtered = df_sorted[(df_sorted['RCS_ID'] == row['RCS_ID']) & ((df_sorted['Duration unlimited'] == 'TRUE') | (df_sorted['Duration limited'] == 'TRUE') | (df_sorted['Duration extended'] == 'TRUE'))]
 
@@ -45,33 +44,40 @@ def page_main():
                 latest.at[index, 'Duration extended'] = filtered.iloc[0]['Duration extended']
                 latest.at[index, 'Duration end date'] = filtered.iloc[0]['Duration end date']
             
-        latest[date_column] = latest[date_column].dt.strftime('%m/%d/%Y')
+        latest[date_column] = datetime.now().date().strftime('%m/%d/%Y')
 
     return render_template('pages/main.html', headers = latest.columns.tolist(), data = latest.values)
 
 @pages.route('/main/kpis')
 @login_required
 def page_main_kpis():
+    dp_fields = ['DP_500', 'DP_016', 'DP_601', 'DP_602', 'DP_603', 'DP_609', 'DP_604', 'DP_605', 'DP_606', 'DP_607', 'DP_608']
     df = pd.read_csv('../../Sample_Data.csv', encoding='iso-8859-1')
-    df['Internal ID'] = ''
-    extract = request.args.get("extract")
+    df = df.query('DP_ID in @dp_fields')
 
-    if extract == 'true':
-        latest_data_per_field_id = df
-    else:
-        date_column = 'Filing_Date'
-        df[date_column] = pd.to_datetime(df[date_column])
-        df_sorted = df.sort_values(by=date_column, ascending=False)
-        latest_data_per_field_id = df_sorted.groupby('RCS_ID').first().reset_index()
-        latest_data_per_field_id[date_column] = latest_data_per_field_id[date_column].dt.strftime('%m/%d/%Y')
+    pivot_df = df.pivot_table(index=['RCS_ID', 'Filing_Date'], columns='DP_Name', values='DP_Value', aggfunc='first').reset_index()
+    pivot_df['Filing_Date'] = pd.to_datetime(pivot_df['Filing_Date'], format='mixed')
+    df_sorted = pivot_df.sort_values(by='Filing_Date', ascending=False)
+    latest = df_sorted.groupby('RCS_ID').first().reset_index()
+    latest['Control'] = 'Mandate Not Renewed! '
+    latest[date_column] = datetime.now().date().strftime('%m/%d/%Y')
 
-    return render_template('pages/main-kpis.html', headers = latest_data_per_field_id.columns.tolist(), data = latest_data_per_field_id.values)
+    for index, row in latest.iterrows():
+        filtered = df_sorted[(df_sorted['RCS_ID'] == row['RCS_ID']) & ((df_sorted['Mandate: Limited'] == 'TRUE') | (df_sorted['Mandate: Unlimited'] == 'TRUE'))]
+
+        if filtered.empty == False:
+            latest.at[index, 'Mandate: Limited'] = filtered.iloc[0]['Mandate: Limited']
+            latest.at[index, 'Mandate: Unlimited'] = filtered.iloc[0]['Mandate: Unlimited']
+            latest.at[index, 'Mandate: Expiration (Date)'] = filtered.iloc[0]['Mandate: Expiration (Date)']
+            latest.at[index, 'Mandate: Expiration (AGM)'] = filtered.iloc[0]['Mandate: Expiration (AGM)']
+
+    return render_template('pages/main-kpis.html', headers = latest.columns.tolist(), data = latest.values)
 
 @pages.route('/main/history')
 @login_required
 def page_main_history():
     df = pd.read_csv('../../Sample_Data.csv', encoding='iso-8859-1')
-    df = df.drop(['Only API', 'SQL_002', 'DP_ID', 'Unique_ID'], axis=1)
+    df = df.drop(['DP_ID', 'Unique_ID'], axis=1)
     return render_template('pages/main-history.html', headers = df.columns.tolist(), data = df.values)
 
 @pages.route('/entity')
@@ -87,7 +93,7 @@ def page_entity():
     pivot_df[date_column] = pd.to_datetime(pivot_df[date_column])
     df_sorted = pivot_df.sort_values(by=date_column, ascending=False)
     latest = df_sorted.groupby('RCS_ID').first().reset_index()
-    latest[date_column] = latest[date_column].dt.strftime('%m/%d/%Y')
+    latest[date_column] = datetime.now().date().strftime('%m/%d/%Y')
     
     return render_template('pages/entity.html', headers = latest.columns.tolist(), data = latest.values)
 
@@ -104,7 +110,7 @@ def page_entity_accounting():
     pivot_df[date_column] = pd.to_datetime(pivot_df[date_column], format='mixed')
     df_sorted = pivot_df.sort_values(by=date_column, ascending=False)
     latest = df_sorted.groupby('RCS_ID').first().reset_index()
-    latest[date_column] = latest[date_column].dt.strftime('%m/%d/%Y')
+    latest[date_column] = datetime.now().date().strftime('%m/%d/%Y')
 
     for index, row in latest.iterrows():
         filtered = df_sorted[(df_sorted['RCS_ID'] == row['RCS_ID']) & ((df_sorted['Capital Fixed'] == 'TRUE') | (df_sorted['Capital Variable'] == 'TRUE'))]
@@ -112,7 +118,7 @@ def page_entity_accounting():
         if filtered.empty == False:
             latest.at[index, 'Capital Fixed'] = filtered.iloc[0]['Capital Fixed']
             latest.at[index, 'Capital Variable'] = filtered.iloc[0]['Capital Variable']
-            latest.at[index, 'Capital amount'] = filtered.iloc[0]['Capital amount']
+            # latest.at[index, 'Capital amount'] = filtered.iloc[0]['Capital amount']
     
     return render_template('pages/entity-accounting.html', headers = latest.columns.tolist(), data = latest.values)
 
@@ -124,7 +130,7 @@ def page_entity_registrations():
 @pages.route('/entity/smo')
 @login_required
 def page_entity_smo():
-    dp_fields = ['DP_011', 'DP_012', 'DP_039', 'DP_040', 'DP_041', 'DP_042', 'DP_043', 'DP_013', 'DP_014', 'DP_015', 'DP_044', 'DP_045', 'DP_049', 'DP_050', 'DP_033', 'DP_035', 'DP_034', 'DP_032', 'DP_037', 'DP_038', 'DP_036']
+    dp_fields = ['DP_600', 'DP_601' 'DP_012', 'DP_011', 'DP_039', 'DP_040', 'DP_041', 'DP_042', 'DP_043', 'DP_013', 'DP_014', 'DP_015', 'DP_044', 'DP_045', 'DP_049', 'DP_050', 'DP_033', 'DP_035', 'DP_034', 'DP_032', 'DP_037', 'DP_038', 'DP_036']
     df = pd.read_csv('../../Sample_Data.csv', encoding='iso-8859-1')
     df1 = df.query('DP_ID in @dp_fields')
 
@@ -132,7 +138,9 @@ def page_entity_smo():
     pivot_df1['Filing_Date'] = pd.to_datetime(pivot_df1['Filing_Date'], format='mixed')
     df_sorted1 = pivot_df1.sort_values(by='Filing_Date', ascending=False)
     latest1 = df_sorted1.groupby('RCS_ID').first().reset_index()
-    latest1['Filing_Date'] = latest1['Filing_Date'].dt.strftime('%m/%d/%Y')
+    latest1['Filing_Date'] = datetime.now().date().strftime('%m/%d/%Y')
+    latest1['Entity Name']  = 'FALSE'
+    latest1['Registration Number']  = 'FALSE'
 
     for index, row in latest1.iterrows():
         filtered = df_sorted1[(df_sorted1['RCS_ID'] == row['RCS_ID']) & ((df_sorted1['Mandate: limited'] == 'TRUE') | (df_sorted1['Mandate: unlimited'] == 'TRUE'))]
@@ -144,31 +152,32 @@ def page_entity_smo():
             latest1.at[index, 'Mandate: expiration (Date)'] = filtered.iloc[0]['Mandate: expiration (Date)']
             latest1.at[index, 'Mandate: expiration (date of AGM)'] = filtered.iloc[0]['Mandate: expiration (date of AGM)']
 
-    dp_fields1 = ['DP_016', 'DP_017', 'DP_018', 'DP_019', 'DP_039', 'DP_040', 'DP_041', 'DP_042', 'DP_043', 'DP_044', 'DP_045', 'DP_49', 'DP_050', 'DP_033', 'DP_035', 'DP_034', 'DP_032', 'DP_037', 'DP_038', 'DP_036']
-    df2 = df.query('DP_ID in @dp_fields')
-
-    pivot_df2 = df2.pivot_table(index=['RCS_ID', 'Filing_Date'], columns='DP_Name', values='DP_Value', aggfunc='first').reset_index()
-    pivot_df2['Filing_Date'] = pd.to_datetime(pivot_df2['Filing_Date'], format='mixed')
-    df_sorted2 = pivot_df2.sort_values(by='Filing_Date', ascending=False)
-    latest2 = df_sorted2.groupby('RCS_ID').first().reset_index()
-    latest2['Filing_Date'] = latest2['Filing_Date'].dt.strftime('%m/%d/%Y')
-
-    for index, row in latest2.iterrows():
-        filtered = df_sorted2[(df_sorted2['RCS_ID'] == row['RCS_ID']) & ((df_sorted2['Mandate: limited'] == 'TRUE') | (df_sorted2['Mandate: unlimited'] == 'TRUE'))]
-
-        if filtered.empty == False:
-            latest2.at[index, 'Mandate: limited'] = filtered.iloc[0]['Mandate: limited']
-            latest2.at[index, 'Mandate: unlimited'] = filtered.iloc[0]['Mandate: unlimited']
-            latest2.at[index, 'Mandate: appointment / renewal date'] = filtered.iloc[0]['Mandate: appointment / renewal date']
-            latest2.at[index, 'Mandate: expiration (Date)'] = filtered.iloc[0]['Mandate: expiration (Date)']
-            latest2.at[index, 'Mandate: expiration (date of AGM)'] = filtered.iloc[0]['Mandate: expiration (date of AGM)']
-
-    return render_template('pages/entity-smo.html', headers1 = latest1.columns.tolist(), data1 = latest1.values, headers2 = latest2.columns.tolist(), data2 = latest2.values)
+    return render_template('pages/entity-smo.html', headers = latest1.columns.tolist(), data = latest1.values)
 
 @pages.route('/entity/kpis')
 @login_required
 def page_entity_kpis():
-    return render_template('pages/entity-kpis.html')
+    dp_fields = ['DP_601', 'DP_602', 'DP_603', 'DP_609', 'DP_604', 'DP_605', 'DP_606', 'DP_607', 'DP_608']
+    df = pd.read_csv('../../Sample_Data.csv', encoding='iso-8859-1')
+    df = df.query('DP_ID in @dp_fields')
+
+    pivot_df = df.pivot_table(index=['RCS_ID', 'Filing_Date'], columns='DP_Name', values='DP_Value', aggfunc='first').reset_index()
+    pivot_df['Filing_Date'] = pd.to_datetime(pivot_df['Filing_Date'], format='mixed')
+    df_sorted = pivot_df.sort_values(by='Filing_Date', ascending=False)
+    latest = df_sorted.groupby('RCS_ID').first().reset_index()
+    latest['Control'] = 'Mandate Not Renewed! '
+    latest[date_column] = datetime.now().date().strftime('%m/%d/%Y')
+
+    for index, row in latest.iterrows():
+        filtered = df_sorted[(df_sorted['RCS_ID'] == row['RCS_ID']) & ((df_sorted['Mandate: Limited'] == 'TRUE') | (df_sorted['Mandate: Unlimited'] == 'TRUE'))]
+
+        if filtered.empty == False:
+            latest.at[index, 'Mandate: Limited'] = filtered.iloc[0]['Mandate: Limited']
+            latest.at[index, 'Mandate: Unlimited'] = filtered.iloc[0]['Mandate: Unlimited']
+            latest.at[index, 'Mandate: Expiration (Date)'] = filtered.iloc[0]['Mandate: Expiration (Date)']
+            latest.at[index, 'Mandate: Expiration (AGM)'] = filtered.iloc[0]['Mandate: Expiration (AGM)']
+
+    return render_template('pages/entity-kpis.html', headers = latest.columns.tolist(), data = latest.values)
 
 @pages.route('/entity/files')
 @login_required
